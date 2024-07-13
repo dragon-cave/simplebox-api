e1787ace1381edbf43dd9e0d7929159fc0c6235a
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-07-13 15:11:55 -0300

    Add default test secret key

--- s3django/settings.py
+++ s3django/settings.py
@@ -109,6 +109,7 @@ if "test" in sys.argv:
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    SECRET_KEY = 'test_secret_key'
 
 AUTH_PASSWORD_VALIDATORS = [
     # {
