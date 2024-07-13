6ec82930d192844776b00701ed2ad1d3d24b901d
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-07-13 15:15:10 -0300

    Add default test secret key

--- s3django/settings.py
+++ s3django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "testsecretkey" if "test" in sys.argv else os.getenv("SECRET_KEY")
+SECRET_KEY = "test secret key" # os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
