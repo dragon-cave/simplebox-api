9497e5c1247b8e7c4a0aedac5551f3f1a2dcfade
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-07-13 15:20:55 -0300

    Fix workflow action file for Django tests

--- s3django/settings.py
+++ s3django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "test secret key" # os.getenv("SECRET_KEY")
+SECRET_KEY = "test secret key" if len(str(os.getenv("TEST_ENV"))) > 0 else os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -104,7 +104,7 @@ DATABASES = {
     }
 }
 
-if "test" in sys.argv:
+if len(str(os.getenv("TEST_ENV"))) > 0:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
