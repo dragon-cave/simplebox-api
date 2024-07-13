64223e707b7b06b7320c5f06b3ede09815cc6b0c
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-07-13 15:05:02 -0300

    Add test workflow and user tests

--- s3django/settings.py
+++ s3django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,6 +103,12 @@ DATABASES = {
         'PORT': os.getenv("DB_PORT"),
     }
 }
+
+if "test" in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     # {
