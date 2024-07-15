3ac009fe579316b1d7c6a95dc044f5933b9ae5fd
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-07-15 19:41:00 -0300

    Add API documentation with drf-spectacular closes #10

--- s3django/urls.py
+++ s3django/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user.views import UserView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/", UserView.as_view(), name="user"),
     path('api/auth/', include('user.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
