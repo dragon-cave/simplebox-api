d75f6abb57b0d9f98bace6159153e7c849ccd366
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-08 21:52:50 -0300

    Fix endpoints, again

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from user.views import UserView, ProfilePictureView
 
 urlpatterns = [
-    path("/", UserView.as_view(), name="user"),
-    path("/profile_picture/", ProfilePictureView.as_view(), name="profile_picture"),
+    path("", UserView.as_view(), name="user"),
+    path("profile_picture/", ProfilePictureView.as_view(), name="profile_picture"),
 ]
