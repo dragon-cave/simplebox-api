aae136b82e568e3c8deca8f6acec8eac076ecce1
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-09 23:28:16 -0300

    Update file URLs to include '/list/' path

--- files/urls.py
+++ files/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r'', FileViewSet, basename='file')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('list/', include(router.urls)),
     path('upload/', FileUploadView.as_view(), name='file-upload'),
     path('webhook/', WebhookView.as_view(), name='webhook'),
 ]
