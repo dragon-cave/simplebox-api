4e558f7a3f019a9228c4915218735cacee3a2b94
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-10 14:31:08 -0300

    Refactor IsPrivateSubnet permission to remove AllowAny and raise PermissionDenied exception for denied access

--- files/views.py
+++ files/views.py
@@ -108,7 +108,7 @@ class FileViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class WebhookView(APIView):
-    permission_classes = [AllowAny, IsPrivateSubnet]
+    permission_classes = [IsPrivateSubnet]
     
     def post(self, request, *args, **kwargs):
         # Process the webhook data here
