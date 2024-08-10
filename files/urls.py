ddddf49387a13aa49503a5eaf5fad87645aa55f3
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-10 13:30:41 -0300

    Refactor file URLs to remove unnecessary FileUploadView import

--- files/urls.py
+++ files/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import FileUploadView, FileViewSet, WebhookView
+from .views import FileViewSet, WebhookView
 
 router = DefaultRouter()
 router.register(r'files', FileViewSet, basename='file')
