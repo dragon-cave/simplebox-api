bc5f81ed0c3592d8956a3a15847a8adc43b46bed
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-10 23:14:09 -0300

    Fix

--- simplebox/settings.py
+++ simplebox/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
     }
 }
 
-if len(str(os.getenv("TEST_ENV"))) > 0:
+if os.getenv("TEST_ENV") != None:
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
