86bd79781c2a26516358a730c1ca164e2f21bcb1
Ryan Sousa <30508456+rfsousa@users.noreply.github.com>
2024-08-10 21:49:38 -0300

    Refactor file routing to use 'files' prefix in URLs

--- simplebox/urls.py
+++ simplebox/urls.py
@@ -5,7 +5,7 @@ from files.views import WebhookView
 urlpatterns = [
     path("api/user/", include('user.urls')),
     path('api/auth/', include('user.auth_urls')),
-    path('api/files/', include('files.urls')),
+    path('api/files', include('files.urls')),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
